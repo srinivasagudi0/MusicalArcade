63902d7785902feb3f66f699e7871d775b09eeeb
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-04-18 14:38:33 -0500

    placeholder

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# i dont know what games to add the games will now only be placeholder and I will search for games to add later
+
+st.title("Game Library")
+st.write("deciding games...")
