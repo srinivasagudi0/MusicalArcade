20b154e04c5a5ce79c4c2aac63151140efa023d8
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-04-18 15:23:39 -0500

    Change selected game from 'Beat Tap' to 'Guess the Song'
    Updated game selection in the sidebar from 'Beat Tap' to 'Guess the Song' and added a placeholder comment for the new game.

--- app.py
+++ app.py
@@ -7,16 +7,6 @@ st.title("Game Library")
 st.write("deciding games...")
 
 st.sidebar.title("What game are you looking for?")
-game_choice = st.sidebar.selectbox("Select a game", ["Beat Tap"])
-
-if game_choice == "Beat Tap":
-    '''
-    simplest music-related option:
-
-Play a song beat track
-Show a cue (flash/circle) on each beat
-Player taps in time
-Score based on timing accuracy (Perfect/Good/Miss)
-
-THis will be the first game I add to the library, I will add more games later on but for now this is the only game in the library
-    '''
+game_choice = st.sidebar.selectbox("Select a game", ["Guess the Song"])
+if game_choice="Guess the Song":
+    # It is goign to be good and simple
