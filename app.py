ce1c4d797bac6add14b60ab98460e5e7778eda0c
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-04-18 15:58:25 -0500

    Fix game choice condition and add random song feature

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import time
+from support import get_random_song
 
 # i dont know what games to add the games will now only be placeholder and I will search for games to add later
 
@@ -8,5 +9,8 @@ st.write("deciding games...")
 
 st.sidebar.title("What game are you looking for?")
 game_choice = st.sidebar.selectbox("Select a game", ["Guess the Song"])
-if game_choice="Guess the Song":
-    # It is goign to be good and simple
+if game_choice == "Guess the Song":
+    # It is going to be good and simple
+    st.header("Guess the Song")
+    st.write("See the lyrics and guess the song!")
+    st.write(get_random_song())
